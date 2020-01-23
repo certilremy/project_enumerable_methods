62437b4ad340fb3d522da16b573ef6317dc563e3
certilremy <certilremy@gmail.com>
2020-01-23 18:13:28 -0500

    added my_each with index and fixed linters error

--- lib/main.rb
+++ lib/main.rb
@@ -7,4 +7,12 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    element = 0
+    while element < length
+      yield(self[element], element)
+      element += 1
+    end
+  end
 end
