b11d3c20e1584c0215a21d69ebbbc22ad738614f
certilremy <certilremy@gmail.com>
2020-02-14 11:38:38 -0500

    fixed the loop entire array

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/main.rb'
 describe Enumerable do
   let(:arr) { [2, 3, 4] }
   let(:new_arr) { [1, 2, 2] }
-  let(:empyt_arr) { [2, 3, 4] }
+  let(:empyt_arr) { [] }
   describe '#my_each' do
     it 'return enumerator if block not given' do
       expect(arr.my_each.class).to eq(Enumerator)
@@ -11,7 +11,7 @@ describe Enumerable do
 
     it 'loops an entire array' do
       new_arr.my_each { |n| empyt_arr[n] = n + 2 }
-      expect(empyt_arr).to eq([2, 3, 4])
+      expect(empyt_arr).to eq([nil, 3, 4])
     end
 
     it 'returns product of each item in array' do
